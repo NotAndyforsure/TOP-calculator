35a60a44996c33d8e6d93b342d39218c8b0b2f5f
Andy <agcmall05@gmail.com>
2022-11-04 00:34:54 +0100

    All functionalities added
    shows result
    keeps operating continuously with the result
    important buttons highlighted with different colours
    padding and screen colour added

--- index.js
+++ index.js
@@ -1,38 +1,90 @@
 
-function resFind(symb,num1,num2){
+function findRes(symb,num1,num2){
 
-    switch(symb){
+    if(typeof num1==="number" && typeof num2==="number"){
+
+        let x=0
+
+        switch(symb){
+
+            
+            case "+":
+                
+                x=num1 + num2
+                return x
+            
+            break;
+
+            case "-":
+
+                x=num1 - num2
+
+                return x
+            
+            break;
+
+            case "x":
+
+                x=num1 * num2
+
+                return x
+            
+            break;
+
+            case "%":
+
+                x=num1/100 * num2
+
+                return x
+            
+            break;
+
+            case "÷":
+                x=num1/num2
+                return x
+            break;
+
+        }
+
+                
+    }else{
+
+        howMannyAriSymb=0
+        res.textContent="0"
+        num1=0
+        num2=0
+        result=0
+
+
+       return "ERROR!"
+
+
         
-        case "+":
-            return num1 + num2
-        break;
-
-        case "-":
-           return num1 - num2
-        break;
-
-        case "*":
-            return num1 + num2
-        break;
-
-        case "%":
-            return (num1/100) * num2
-        break;
-
-        case "÷":
-            return num1/num2
-        break;
 
     }
+
+       
 }
 
 
 // Arithmatic engine//////////////////////////////////////////////////////////////////////
 
 
+
+
+
+
+
+
+
 let negVar=""
 let howMannyAriSymb=0
 let calcres=0
+let num1=0
+let num2=0
+let symb=""
+let result=0
+let wait="off"
 
 
 
@@ -40,6 +92,20 @@ let calcres=0
 let button=document.querySelectorAll(".cell")
 let screen=document.querySelector("#calc-screen")
 let res=document.querySelector("#calc-res")
+
+
+//Variables bank////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -50,6 +116,8 @@ button.forEach((ele)=>
         e.stopPropagation()
 
         if(this.dataset.value===undefined){
+
+            wait="on"
 
             if(negVar=="" && this.textContent=="(-)" || negVar[negVar.length-1]===" "){
 
@@ -78,19 +146,59 @@ button.forEach((ele)=>
 
 
 
-        if(this.dataset.value==1 && negVar!="" && howMannyAriSymb ==0){
 
-            howMannyAriSymb=1
 
-            negVar+=` ${this.textContent} `
 
-            return screen.textContent+=` ${this.textContent} `
+
+
+
+
+        if(this.dataset.value==1){
+           
+
+            if(negVar!="" && howMannyAriSymb ==0){
+
+                howMannyAriSymb=1
+
+                negVar+=` ${this.textContent} `
+
+    
+                return screen.textContent+=` ${this.textContent} `
+
+
+
+
+            }else if(negVar==="" && howMannyAriSymb == 0 && result!=0 && wait==="off"){
+
+                howMannyAriSymb=1
+
+                negVar=`${result}` + ` ${this.textContent} `
+    
+                return screen.textContent=`${res.textContent}` + ` ${this.textContent} `
+
+
+            }
+
+          
+
 
 
         }
 
 
-        //Show Arithmetic symbol////////////////////////////////////////////////////////////////////////////
+        //Arithmetic operator symbol behavior////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -125,7 +233,12 @@ button.forEach((ele)=>
 
                 negVar=`${screen.textContent.substring(0,strLength - 1)}` 
 
-                return screen.textContent=`${negVar}`   
+                screen.textContent=`${negVar}`
+                
+                if(negVar.length===0){
+
+                    wait="off"
+                }
 
             }
 
@@ -140,12 +253,30 @@ button.forEach((ele)=>
 
 
 
-        if(this.dataset.value == 4 && negVar != ""){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if(this.dataset.value == 4){
 
             howMannyAriSymb=0
             negVar=""
             screen.textContent=""
             res.textContent="0"
+            num1=0
+            num2=0
+            result=0
 
         }
 
@@ -158,16 +289,98 @@ button.forEach((ele)=>
 
 
 
+
+
+
+
+
+
+
+
+
+
+        if(this.dataset.value==2){
+
+            if(negVar.split(" ").length==3){
+
+
+                let splitter=negVar.split(" ")
+                
+                num1=parseFloat(splitter[0])
+                num2=parseFloat(splitter[2])
+
+
+
+                if(splitter[0].substring(0,1)=="("){
+
+                    let x= splitter[0].substring(3,splitter[0].length)
+
+                    num1=-parseInt(x)
+                
+                }
+
+                if(splitter[2].substring(0,1)=="("){
+
+
+                    
+                    let y= splitter[2].substring(3,splitter[2].length)
+
+                    num2=-parseInt(y)
+
+        
+                }
+
+
+
+
+                
+                result=findRes(splitter[1],num1,num2)
+
+                if(result>=0){
+
+                    res.textContent=`${result}`
+                    howMannyAriSymb=0
+                    screen.textContent=""
+                    negVar=""
+                    wait="off"
+                }else{
+
+                    let negResult=result.toString()
+
+                    res.textContent=`(-)${negResult.substring(1,negResult.length)}`
+                    howMannyAriSymb=0
+                    screen.textContent=""
+                    negVar=""
+                    wait="off"
+
+                }
+               
+               
+        
+
+
+            }else{
+
+                return screen.textContent=findRes("a","a","a")
+           
+            }
+
+                
+        }
+
+
+
+        //Equal sign behavior//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
     })
 )
 
 
 
 
-
-
-
-//screen typing function////////////////////////////////////////////////////////////////////////////////
 
 
 
